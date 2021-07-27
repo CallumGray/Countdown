9d18d4a80e916618a1be80687e174be293d4abfc
CallumGray <callumxgray@gmail.com>
2021-07-27 03:23:13 +0100

    Numbers solver now gives an expression

--- CountdownNumbers.py
+++ CountdownNumbers.py
@@ -1,4 +1,5 @@
 from typing import List
+from typing import Tuple
 import random
 
 #
@@ -31,9 +32,6 @@ def target() -> int:
 
 samples: List[int] = sample_numbers(2, 4)
 
-for i in samples:
-    print(i)
-
 target_num = target()
 print("TARGET: ", target_num)
 
@@ -48,12 +46,17 @@ class NumbersSolver:
 
     def __init__(self):
         self.best: int = 0
+        self.best_expr: str = ''
         self.counter: int = 0
 
     # See if the number is the closest to the target
-    def compare_to_best(self, n: int, target: int):
-        if abs(target - n) < abs(target - self.best):
-            self.best = n
+    def compare_to_best(self, n: Tuple[int,str], target: int):
+        num = n[0]
+        expr = n[1]
+
+        if abs(target - num) < abs(target - self.best):
+            self.best = num
+            self.best_expr = expr
 
     # Custom function similar to itertools.combinations(xs,2), but also
     # yields the indexes of each combination from within the original list
@@ -64,32 +67,40 @@ class NumbersSolver:
                     yield (xi, xj, i, j)
 
     # Get list of all values that can be made from 2 integers
-    def operation_results(self, a: int, b: int) -> List[int]:
+    def operation_results(self, a: Tuple[int,str], b: Tuple[int,str]) -> List[Tuple[int,str]]:
+
+        # Numeric value
+        a_num = a[0]
+        b_num = b[0]
+
+        # Mathematical expression leading up to numeric value
+        a_expr = a[1]
+        b_expr = b[1]
 
         # Order doesn't matter for addition and multiplication
-        add: int = a + b
-        mult: int = a * b
+        add: Tuple[int,str] = (a_num + b_num,'('+a_expr+'+'+b_expr+')')
+        mult: Tuple[int,str] = (a_num * b_num,'('+a_expr+'*'+b_expr+')')
 
         # Order matters for subtraction and division
-        (bigger, smaller) = (a, b) if a > b else (b, a)
+        (bigger, smaller) = ((a_num,a_expr), (b_num,b_expr)) if a_num > b_num else ((b_num,b_expr), (a_num,a_expr))
 
-        sub: int = bigger - smaller
+        sub: Tuple[int,str] = (bigger[0] - smaller[0],'('+bigger[1]+'-'+smaller[1]+')')
 
         self.counter += 3
 
         # Division is invalid if divisor is 0 or it doesn't result in an integer
-        if smaller == 0 or bigger % smaller != 0:
+        if smaller[0] == 0 or bigger[0] % smaller[0] != 0:
             return [add, sub, mult]
 
-        div: int = bigger // smaller
+        div: Tuple[int,str] = (bigger[0] // smaller[0],'('+bigger[1]+'/'+smaller[1]+')')
         self.counter += 1
 
         return [add, sub, mult, div]
 
     # All operation_results between 2 lists
-    def list_operations(self, alist: List[int], blist: List[int]) -> List[int]:
+    def list_operations(self, alist: List[Tuple[int,str]], blist: List[Tuple[int,str]]) -> List[Tuple[int,str]]:
 
-        final_list: List[int] = []
+        final_list: List[Tuple[int,str]] = []
 
         for a in alist:
             for b in blist:
@@ -98,21 +109,22 @@ class NumbersSolver:
         return final_list
 
     # Set best to default and set the numbers to the form [[x],[y],[z] ... ]
-    def initialise(self, numbers: List[int], target: int) -> List[List[int]]:
+    def initialise(self, numbers: List[int], target: int) -> List[List[Tuple[int,str]]]:
 
         # set best to 0
         self.best = 0
+        self.best_expr = ''
 
         # Using all of the numbers on their own
         for n in numbers:
-            self.compare_to_best(n, target)
+            self.compare_to_best((n,str(n)), target)
 
         # return numbers as a list of lists [[x],[y],[z] ... ]
-        return [[n] for n in numbers]
+        return [[(n,str(n))] for n in numbers]
 
     # Numbers must be supplied in the form [[x],[y],[z] ... ] to simplify the algorithm.
     # Since list_operations returns a list, the values in numbers must be lists too.
-    def calculate_results(self, numbers: List[List[int]], target: int):
+    def calculate_results(self, numbers: List[List[Tuple[int,str]]], target: int):
 
         if len(numbers) > 1:
             # Get all pairs of lists within the list
@@ -133,13 +145,12 @@ class NumbersSolver:
 
                 # Call function again with new_numbers_list
                 self.calculate_results(new_numbers_list, target)
-        # else:
-        #     print(numbers)
 
 
 num_solver = NumbersSolver()
 numbers_list = num_solver.initialise(samples,target_num)
-print(numbers_list)
+print([n[0][0] for n in numbers_list])
 num_solver.calculate_results(numbers_list,target_num)
-print(num_solver.best)
-print(num_solver.counter)
+print('Best:',num_solver.best)
+print(num_solver.best_expr)
+print('Calculations:',num_solver.counter)
